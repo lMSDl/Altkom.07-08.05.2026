992c48f3db789516f84757fe64e301263467be74
Paweł <pawel@msd.info.pl>
2026-05-07 09:46:09 +0200

    Przygotowanie projektu

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+using DAL;
+using Microsoft.EntityFrameworkCore;
+
+var config = new DbContextOptionsBuilder<Context>()
+    .UseSqlServer("Server=(local);Database=EF;TrustServerCertificate=True;Integrated Security=true");
+
+using (var context = new Context(config.Options))
+{
+    context.Database.EnsureDeleted();
+    context.Database.EnsureCreated();
+}
---
+++ DAL/Context.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class Context : DbContext
+    {
+
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+
+        override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
